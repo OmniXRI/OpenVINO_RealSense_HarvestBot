6d067697a334d3e02c6eed1d1098d319f5f348b3
OmniXRI <32646688+OmniXRI@users.noreply.github.com>
2019-11-12 11:21:37 +0800

    Add files via upload

--- my_realsense/rs-opencvshow.cpp
+++ my_realsense/rs-opencvshow.cpp
@@ -1,120 +1,66 @@
-﻿// License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+#include <librealsense2/rs.hpp> // 引入RealSense標頭檔
+#include <opencv2/opencv.hpp>   // 引入OpenCV標頭檔
 
-// include the librealsense C++ header file
-#include <librealsense2/rs.hpp>
+using namespace std; // 使用std標準函式命名空間
+using namespace cv;  // 使用OpenCV函式命名空間
 
-// include OpenCV header file
-#include <opencv2/opencv.hpp>
-
-using namespace std;
-using namespace cv;
-
-void my_video_play()
-{
-	VideoCapture my_cap("my_video.mpg");
-	Mat frame;
-
-	namedWindow("my video play");
-
-	if (!my_cap.isOpened()){
-		cout << "Read video failed !" << endl;
-		exit(0);
-	}
-
-	int frame_num = my_cap.get(CAP_PROP_FRAME_COUNT);
-	double frame_rate = my_cap.get(CV_CAP_PROP_FPS);
-	int frame_delay = 1000 / frame_rate;
-
-	cout << "total frame number is: " << frame_num << endl;
-	cout << "FPS = " << frame_rate << endl;
-
-	for (int i = 0;i < frame_num;i++) {
-		my_cap >> frame;
-		imshow("my video play", frame);
-
-		//char img_name[15];
-		//
-		//if (i % 10 == 0) {
-		//	sprintf(img_name, "img_%d.jpg", (2000 + i));
-		//	imwrite(img_name, frame);
-		//}		
-
-		if (waitKey(frame_delay) == 'q') {
-			break;
-		}
-	}
-
-	destroyWindow("my video play");
-	my_cap.release();
-	cout << "play done." << endl;
-}
-
+// 主程式入口
 int main(int argc, char * argv[]) try
 {
-	//Contruct a pipeline which abstracts the device
-	rs2::pipeline pipe;
+  // 建構一個RealSense抽象設備的管道以容納擷取到的影像
+  rs2::pipeline pipe;
 
-	//Create a configuration for configuring the pipeline with a non default profile
-	rs2::config cfg;
+  // 創建自定義參數以配置管道
+  rs2::config cfg;
 
-	// setting frame size (width and height)
-	const int w = 640;
-	const int h = 480;
+  // 設定影像尺寸(寬w，高h)
+  const int w = 640;
+  const int h = 480;
 
-	//Add desired streams to configuration
-	cfg.enable_stream(RS2_STREAM_COLOR, w, h, RS2_FORMAT_BGR8, 30);
-	cfg.enable_stream(RS2_STREAM_DEPTH, w, h, RS2_FORMAT_Z16, 30);
-	cfg.enable_stream(RS2_STREAM_INFRARED, 1, w, h, RS2_FORMAT_Y8, 30); // left
-	cfg.enable_stream(RS2_STREAM_INFRARED, 2, w, h, RS2_FORMAT_Y8, 30); // right	
+  // 設定欲顯示的影像流(可依需求啟動不一定要全設)
+  cfg.enable_stream(RS2_STREAM_COLOR, w, h, RS2_FORMAT_BGR8, 30); // BGR888格式彩色影像 30fps
+  cfg.enable_stream(RS2_STREAM_DEPTH, w, h, RS2_FORMAT_Z16, 30); // 16 bit格式灰階深度影像 30fps
+  cfg.enable_stream(RS2_STREAM_INFRARED, 1, w, h, RS2_FORMAT_Y8, 30); // 8 bit格式左紅外線影像 30fps
+  cfg.enable_stream(RS2_STREAM_INFRARED, 2, w, h, RS2_FORMAT_Y8, 30); // 8 bit格式右紅外線影像 30fps
 
-	//Instruct pipeline to start streaming with the requested configuration
-	pipe.start(cfg);
+  // 根據設定值啟動指定串流影像
+  pipe.start(cfg);
 
-	// Declare depth colorizer for pretty visualization of depth data
-	rs2::colorizer color_map;
+  // 宣告深度(灰階)影像著虛擬色彩對應表
+  rs2::colorizer color_map;
 
-	//my_video_play();
+  while (waitKey(1) < 0) // 若有按鍵則結束顯示串流影像
+  {
+    // 等待下一組影像	
+    rs2::frameset frames = pipe.wait_for_frames();
 
-	//VideoWriter my_video("my_video.mpg", CV_FOURCC('D', 'I', 'V', 'X'), 30.0, cv::Size(w, h), true);
-
-	while (waitKey(1) < 0)
-	{
-		// Wait for next set of frames from the camera	
-		rs2::frameset frames = pipe.wait_for_frames();
-				
-	    //Get each frame
-		rs2::frame color_frame = frames.get_color_frame();
-		//rs2::frame depth_frame = frames.get_depth_frame().apply_filter(color_map);
-		//rs2::frame irL_frame = frames.get_infrared_frame(1);
-		//rs2::frame irR_frame = frames.get_infrared_frame(2);		
+    // 取得每一張影像(可依需求不一定要全取)
+    rs2::frame color_frame = frames.get_color_frame(); // 彩色影像
+    rs2::frame depth_frame = frames.get_depth_frame().apply_filter(color_map); // 著虛擬色彩之深度影像
+    rs2::frame irL_frame = frames.get_infrared_frame(1); // 左紅外線灰階影像
+    rs2::frame irR_frame = frames.get_infrared_frame(2); // 右紅外線灰階影像
 		
-		// Creating OpenCV Matrix from a color image
-		Mat color_image(Size(w, h), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
-		//Mat depth_image(Size(w, h), CV_8UC3, (void*)depth_frame.get_data(), Mat::AUTO_STEP);
-		//Mat irL_image(Size(w, h), CV_8UC1, (void*)irL_frame.get_data(), Mat::AUTO_STEP);
-		//Mat irR_image(Size(w, h), CV_8UC1, (void*)irR_frame.get_data(), Mat::AUTO_STEP);		
+    // 建立OpenCV Mat格式之影像(可依需求不一定要全建立)
+    Mat color_image(Size(w, h), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP); // 彩色影像
+    Mat depth_image(Size(w, h), CV_8UC3, (void*)depth_frame.get_data(), Mat::AUTO_STEP); // 著虛擬色彩之深度影像
+    Mat irL_image(Size(w, h), CV_8UC1, (void*)irL_frame.get_data(), Mat::AUTO_STEP); // 左紅外線灰階影像
+    Mat irR_image(Size(w, h), CV_8UC1, (void*)irR_frame.get_data(), Mat::AUTO_STEP); // 右紅外線灰階影像		
 
-		// Display in a GUI
-		imshow("Color Image", color_image);
-		//moveWindow("Color Image", 0, 0);
-		//my_video << color_image;
-		//imshow("Depth Image", depth_image);
-		//moveWindow("Depth Image", w, 0);
-		//imshow("Left IR Image", irL_image);
-		//moveWindow("Left IR Image", 0, h);
-		//imshow("Right IR Image", irR_image);
-		//moveWindow("Right IR Image", w, h);		
-	}
+    // 以OpenCV函式顯示擷取到影像(可依需求不一定要全顯示)
+    imshow("Color Image", color_image); // 彩色影像
+    imshow("Depth Image", depth_image); // 著虛擬色彩之深度影像
+    imshow("Left IR Image", irL_image); // 左紅外線灰階影像
+    imshow("Right IR Image", irR_image); // 右紅外線灰階影像
+  }
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
-catch (const rs2::error & e)
+catch (const rs2::error & e) // 擷取錯誤呼叫碼及顯示
 {
 	std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
 	return EXIT_FAILURE;
 }
-catch (const std::exception& e)
+catch (const std::exception& e) // 擷取意外錯誤及顯示
 {
 	std::cerr << e.what() << std::endl;
 	return EXIT_FAILURE;
